b7986ce7c3b9f000f6ac9eba8f4ef3d66b17ba74
DEBASHIS GARAI <debashis1805@gmail.com>
2024-06-15 23:19:16 +0530

    added some files

---
+++ Frontend_IMA/src/components/data.js
@@ -0,0 +1,156 @@
+export const data = [
+    {
+      checked: false,
+      cust_name: "Andrea James",
+      cust_no: 2523532,
+      inv_no: 73457346735,
+      inv_amnt: "122.87K",
+      due_date: "23-Jan-2021",
+      pred_date: "--",
+      notes:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia adipisci quam iusto omnis eum, itaque optio eligendi veritatis ab dignissimos doloremque tempora nesciunt!",
+    },
+    {
+      checked: false,
+      cust_name: "Andrea James",
+      cust_no: 2523532,
+      inv_no: 73457346735,
+      inv_amnt: "122.87K",
+      due_date: "23-Jan-2021",
+      pred_date: "--",
+      notes:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia adipisci quam iusto omnis eum, itaque optio eligendi veritatis ab dignissimos doloremque tempora nesciunt!",
+    },
+    {
+      checked: false,
+      cust_name: "Andrea James",
+      cust_no: 2523532,
+      inv_no: 73457346735,
+      inv_amnt: "122.87K",
+      due_date: "23-Jan-2021",
+      pred_date: "--",
+      notes:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia adipisci quam iusto omnis eum, itaque optio eligendi veritatis ab dignissimos doloremque tempora nesciunt!",
+    },
+    {
+      checked: false,
+      cust_name: "Andrea James",
+      cust_no: 2523532,
+      inv_no: 73457346735,
+      inv_amnt: "122.87K",
+      due_date: "23-Jan-2021",
+      pred_date: "--",
+      notes:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia adipisci quam iusto omnis eum, itaque optio eligendi veritatis ab dignissimos doloremque tempora nesciunt!",
+    },
+    {
+      checked: false,
+      cust_name: "Andrea James",
+      cust_no: 2523532,
+      inv_no: 73457346735,
+      inv_amnt: "122.87K",
+      due_date: "23-Jan-2021",
+      pred_date: "--",
+      notes:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia adipisci quam iusto omnis eum, itaque optio eligendi veritatis ab dignissimos doloremque tempora nesciunt!",
+    },
+    {
+      checked: false,
+      cust_name: "Andrea James",
+      cust_no: 2523532,
+      inv_no: 73457346735,
+      inv_amnt: "122.87K",
+      due_date: "23-Jan-2021",
+      pred_date: "--",
+      notes:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia adipisci quam iusto omnis eum, itaque optio eligendi veritatis ab dignissimos doloremque tempora nesciunt!",
+    },
+    {
+      checked: false,
+      cust_name: "Andrea James",
+      cust_no: 2523532,
+      inv_no: 73457346735,
+      inv_amnt: "122.87K",
+      due_date: "23-Jan-2021",
+      pred_date: "--",
+      notes:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia adipisci quam iusto omnis eum, itaque optio eligendi veritatis ab dignissimos doloremque tempora nesciunt!",
+    },
+    {
+      checked: false,
+      cust_name: "Andrea James",
+      cust_no: 2523532,
+      inv_no: 73457346735,
+      inv_amnt: "122.87K",
+      due_date: "23-Jan-2021",
+      pred_date: "--",
+      notes:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia adipisci quam iusto omnis eum, itaque optio eligendi veritatis ab dignissimos doloremque tempora nesciunt!",
+    },
+    {
+      checked: false,
+      cust_name: "Andrea James",
+      cust_no: 2523532,
+      inv_no: 73457346735,
+      inv_amnt: "122.87K",
+      due_date: "23-Jan-2021",
+      pred_date: "--",
+      notes:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia adipisci quam iusto omnis eum, itaque optio eligendi veritatis ab dignissimos doloremque tempora nesciunt!",
+    },
+    {
+      checked: false,
+      cust_name: "Andrea James",
+      cust_no: 2523532,
+      inv_no: 73457346735,
+      inv_amnt: "122.87K",
+      due_date: "23-Jan-2021",
+      pred_date: "--",
+      notes:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia adipisci quam iusto omnis eum, itaque optio eligendi veritatis ab dignissimos doloremque tempora nesciunt!",
+    },
+    {
+      checked: false,
+      cust_name: "Andrea James",
+      cust_no: 2523532,
+      inv_no: 73457346735,
+      inv_amnt: "122.87K",
+      due_date: "23-Jan-2021",
+      pred_date: "--",
+      notes:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia adipisci quam iusto omnis eum, itaque optio eligendi veritatis ab dignissimos doloremque tempora nesciunt!",
+    },
+    {
+      checked: false,
+      cust_name: "Andrea James",
+      cust_no: 2523532,
+      inv_no: 73457346735,
+      inv_amnt: "122.87K",
+      due_date: "23-Jan-2021",
+      pred_date: "--",
+      notes:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia adipisci quam iusto omnis eum, itaque optio eligendi veritatis ab dignissimos doloremque tempora nesciunt!",
+    },
+    {
+      checked: false,
+      cust_name: "Andrea James",
+      cust_no: 2523532,
+      inv_no: 73457346735,
+      inv_amnt: "122.87K",
+      due_date: "23-Jan-2021",
+      pred_date: "--",
+      notes:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia adipisci quam iusto omnis eum, itaque optio eligendi veritatis ab dignissimos doloremque tempora nesciunt!",
+    },
+    {
+      checked: false,
+      cust_name: "Andrea James",
+      cust_no: 2523532,
+      inv_no: 73457346735,
+      inv_amnt: "122.87K",
+      due_date: "23-Jan-2021",
+      pred_date: "--",
+      notes:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia adipisci quam iusto omnis eum, itaque optio eligendi veritatis ab dignissimos doloremque tempora nesciunt!",
+    },
+  ];
