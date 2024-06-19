ac25d1cd6cc26827733469279886d1d8a47d44f9
DEBASHIS GARAI <debashis1805@gmail.com>
2024-06-19 17:55:05 +0530

    added some ui changes

--- Frontend_IMA/src/components/RenderTable.jsx
+++ Frontend_IMA/src/components/RenderTable.jsx
@@ -1,4 +1,5 @@
-import {data} from "./data"
+import { data } from "./data";
+import './scrollbar.css'
 
 const headers = [
   "customer name",
@@ -13,34 +14,38 @@ const headers = [
 const RenderTable = () => {
   return (
     <div className="w-full px-7">
-      <table className="w-full h-[660px]">
-        <thead className="w-full">
-          <tr className="border-b-2 border-b-[#283A46] w-full">
-            <th>
-              <input type="checkbox" />
-            </th>
-            {headers.map((e, idx) => (
-              <th key={idx} className="capitalize text-lg text-[#97A1A9]">
-                {e}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((val, idx) => (
-            <tr key={idx} className="text-center text-lg text-white">
-              <input type="checkbox" defaultChecked="false" />
-              <td className="text-center">{val.cust_name}</td>
-              <td className="text-center">{val.cust_no}</td>
-              <td className="text-center">{val.inv_no}</td>
-              <td className="text-center">{val.inv_amnt}</td>
-              <td className="text-center">{val.due_date}</td>
-              <td className="text-center">{val.pred_date}</td>
-              <td className="max-w-[100px] overflow-hidden whitespace-nowrap text-ellipsis text-center">{val.notes}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="w-full h-[660px]  flex flex-col overflow-y-auto">
+        <div className="border-b-2 border-b-[#283A46] w-full flex">
+            <div className="w-full flex pr-[1rem] gap-[9.35rem]">
+          <input type="checkbox" />
+              {headers.map((e, idx) => (
+                <div key={idx} className="capitalize text-lg text-[#97A1A9] text-center">
+                  {e}
+                </div>
+              ))}
+            </div>
+        </div>
+        <div className="flex w-full gap-[1rem] flex-col pr-[1rem]">
+
+        {data.map((val, idx) => (
+          <div
+          key={idx}
+          className="text-center text-lg text-white flex flex-row w-full justify-between"
+          >
+            <input type="checkbox" defaultChecked={false} />
+            <div className="text-center">{val.cust_name}</div>
+            <div className="text-center">{val.cust_no}</div>
+            <div className="text-center">{val.inv_no}</div>
+            <div className="text-center">{val.inv_amnt}</div>
+            <div className="text-center">{val.due_date}</div>
+            <div className="text-center">{val.pred_date}</div>
+            <div className="max-w-[100px] overflow-hidden whitespace-nowrap text-ellipsis text-center">
+              {val.notes}
+            </div>
+          </div>
+        ))}
+        </div>
+      </div>
     </div>
   );
 };
