6fa3d049e48da1ed7d5b3af2732d49fbf88a3dbc
DEBASHIS GARAI <debashis1805@gmail.com>
2024-06-21 03:08:39 +0530

    added the initial server code

--- Backend_IMA/server.js
+++ Backend_IMA/server.js
@@ -1,2 +1,22 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+app.get('/invoices', (req, res) => {
+
+})
+
+app.post('/invoices', (req, res) => {
+
+})
+
+app.use((err, req, res, next) =>{
+    res.status(404).json({
+        message : "Some error occured",
+    })
+})
+app.listen(3000);
+
+
