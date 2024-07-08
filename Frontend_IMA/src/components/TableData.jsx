e396f69ef6b9433bf2b57a23193e55cd0b8ab1b0
DEBASHIS GARAI <debashis1805@gmail.com>
2024-07-08 18:04:59 +0530

    added some coloring feature in the page

--- Frontend_IMA/src/components/TableData.jsx
+++ Frontend_IMA/src/components/TableData.jsx
@@ -1,20 +1,22 @@
 import PropTypes from "prop-types";
-import { useState } from 'react';
+import { useState } from "react";
 
-const TableData = ({ val, setInvID}) => {
+const TableData = ({ val, setInvID }) => {
   const [isChecked, setIsChecked] = useState(false);
 
   // function to handle the checkboxes
   const handleCheck = (idx) => {
     setInvID(idx);
     setIsChecked(!isChecked);
-  }
+  };
 
   return (
-    <div
-      className="text-lg text-white flex flex-row w-full justify-between"
-    >
-      <input type="checkbox" defaultChecked={isChecked} onClick={() => handleCheck(val._id)} />
+    <div className="text-lg text-white flex flex-row w-full justify-between">
+      <input
+        type="checkbox"
+        defaultChecked={isChecked}
+        onClick={() => handleCheck(val._id)}
+      />
       <div className="w-[11%] text-left overflow-hidden whitespace-nowrap text-ellipsis">
         {val.name_customer}
       </div>
@@ -35,7 +37,17 @@ const TableData = ({ val, setInvID}) => {
           : val.Delay + " days"}
       </div>
       <div className="text-left w-[11%] overflow-hidden whitespace-nowrap text-ellipsis capitalize">
-        {val.delay_bucket}
+        {val.delay_bucket === "more than 100 days of delay" ? (
+          <span className="text-[#ff2f2f] font-semibold underline">{val.delay_bucket}</span>
+        ) : val.delay_bucket === "60 to 100 days of delay" ? (
+          <span className="text-[#ff9d5b]">{val.delay_bucket}</span>
+        ) : val.delay_bucket === "45 to 60 days of delay" ||
+          val.delay_bucket === "30 to 45 days of delay" ||
+          val.delay_bucket === "15 to 30 days of delay" ? (
+          <span className="text-[#fffc5b]">{val.delay_bucket}</span>
+        ) : (
+          <span className="text-[#5bff5b]">{val.delay_bucket}</span>
+        )}
       </div>
     </div>
   );
@@ -43,6 +55,6 @@ const TableData = ({ val, setInvID}) => {
 
 TableData.propTypes = {
   val: PropTypes.object.isRequired,
-  setInvID : PropTypes.func.isRequired,
+  setInvID: PropTypes.func.isRequired,
 };
 export default TableData;
